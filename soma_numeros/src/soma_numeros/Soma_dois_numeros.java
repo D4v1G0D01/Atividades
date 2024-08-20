3b21510a0abce616109a50fba2c56f7b6047247d
D4v1G0D01 <161522189+D4v1G0D01@users.noreply.github.com>
2024-08-20 15:08:32 -0300

    Update Soma_dois_numeros.java

--- soma_numeros/src/soma_numeros/Soma_dois_numeros.java
+++ soma_numeros/src/soma_numeros/Soma_dois_numeros.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Soma_dois_numeros {
 	public static void main() {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);//Cria um novo 'Scanner' = 'sc'
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = A + B;
+		int A = sc.nextInt();//Lê a primeira variável
+		int B = sc.nextInt();//Lê a segunda variável
+		int C = A + B;//Da para 'C', o valor da soma de 'A'+ 'B'
 		
 		System.out.printf("%d", C);
-		sc.close();
+		sc.close();//Fecha scanner
 	}
 }
